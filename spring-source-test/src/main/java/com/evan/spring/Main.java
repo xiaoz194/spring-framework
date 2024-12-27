e07ed25edd19ba815e229f78d99ce411edcbe488
xiaoz194 <evanzha@tencent.com>
2024-12-27 15:21:55 +0800

    feat 调试环境搭建

---
+++ spring-source-test/src/main/java/com/evan/spring/Main.java
@@ -0,0 +1,9 @@
+package com.evan.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
+	}
+}
